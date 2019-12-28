68b495c3fad4391568c04dccd01438921d2dbb01
Guru Sarath <gurusarath1.official@gmail.com>
2019-12-28 15:43:02 -0600

    New API

--- Graph_API.c
+++ Graph_API.c
@@ -1,70 +1,158 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef int DATA_TYPE;
+typedef char DATATYPE;
+
+typedef enum errorCodes
+{
+	FAILURE,
+	SUCCESS
+
+}errorCode;
 
 typedef enum Graph_Type
 {
-    DIRECTED,
-    UNDIRECTED
+	DIRECTED,
+	UNDIRECTED
 
 }Graph_Type;
 
 typedef struct GraphVertex GraphVertex;
+
+typedef struct GraphEdge GraphEdge;
+
+struct GraphEdge
+{
+	int ToVertex;
+	double cost;
+	GraphEdge* NextEdge;
+};
+
 struct GraphVertex
 {
-    DATA_TYPE name;
-    GraphVertex* AdjNode;
-
+	DATATYPE* name;
+	int vertexID;
+	GraphEdge* EdgeList;
 };
 
 typedef struct Graph
 {
-    int V,E;
-    Graph_Type type;
-    GraphVertex* VertexArray;
-
+	int V,E;
+	Graph_Type type;
+	GraphVertex* VertexArray;
 } Graph;
+
+
+
+
+
+errorCode CreateEdge(Graph* g_ptr, int startVertex, int endVertex, int cost);
+Graph CreateGraph(DATATYPE vertexNames[], int numVertex, Graph_Type type);
+void printGraph(Graph g);
+
+
+
+
 
 int main()
 {
-    DATA_TYPE x[6] = {4,5,6,7,8,9,0};
-    Graph* Gptr;
-    createGraph(&Gptr, x, 6);
+	DATATYPE x[] = {'A','B','C','D','E','F','G'};
+	int size = sizeof(x)/sizeof(DATATYPE);
 
-    printAllVerticesInGraph(Gptr);
+	Graph g = CreateGraph(x, size, UNDIRECTED);
+	CreateEdge(&g, 0, 2, 1);
+	CreateEdge(&g, 1, 4, 1);
+	CreateEdge(&g, 0, 5, 1);
 
-    return 0;
+	printGraph(g);
+
+	return 0;
 }
 
-int createGraph(Graph** G, DATA_TYPE VertexList[], int numVertex, Graph_Type type_in)
+Graph CreateGraph(DATATYPE vertexNames[], int numVertex, Graph_Type type)
 {
-    *G = (Graph*)malloc(sizeof(Graph));
-    GraphVertex* VertexAryX = (GraphVertex*)malloc(sizeof(GraphVertex) * numVertex);
-    (*G)->V = numVertex;
-    (*G)->type = type_in;
 
-    for(int i=0; i<numVertex; i++)
-    {
-        VertexAryX[i].name = VertexList[i];
-    }
+	Graph g;
+	GraphVertex* v_ary = (GraphVertex*)malloc(sizeof(GraphVertex)*numVertex);
 
-    (*G)->VertexArray = VertexAryX;
+
+	for(int i=0; i<numVertex; i++)
+	{
+		v_ary[i].name = &vertexNames[i];
+		v_ary[i].vertexID = i;
+		v_ary[i].EdgeList = NULL;
+	}
+
+	g.V = numVertex;
+	g.E = 0;
+	g.type = type;
+	g.VertexArray = v_ary;
+
+	return g;
 }
 
-int printAllVerticesInGraph(Graph* G_ptr)
+
+errorCode CreateEdge(Graph* g_ptr, int startVertex, int endVertex, int cost)
 {
 
-    for(int i=0; i<G_ptr->V; i++)
-    {
-        printf("Vertex %d:  %d\n",i+1,G_ptr->VertexArray[i].name);
-    }
-    return 0;
+	GraphEdge* edge = g_ptr->VertexArray[startVertex].EdgeList;
 
+	GraphEdge* prevEdge = edge;
+
+	if(edge != NULL)
+	{
+		printf("\nNULL");
+		while(edge != NULL)
+		{
+			prevEdge = edge;
+			edge = edge->NextEdge;
+		}
+
+	} else {
+
+		printf("\nElse");
+
+
+		prevEdge = (GraphEdge*) malloc(sizeof(GraphEdge));
+		prevEdge->NextEdge = NULL;
+		prevEdge->ToVertex = endVertex;
+		prevEdge->cost = cost;
+
+		g_ptr->E += 1;
+
+		return SUCCESS;
+	}
+
+	prevEdge->NextEdge = (GraphEdge*) malloc(sizeof(GraphEdge));
+	prevEdge->NextEdge->NextEdge = NULL;
+	prevEdge->NextEdge->ToVertex = endVertex;
+	prevEdge->NextEdge->cost = cost;
+
+	g_ptr->E += 1;
+	return SUCCESS;
 }
 
-int addEdgeToGraph(Graph* G, DATA_TYPE from_Vertex, DATA_TYPE to_Vertex)
+void printGraph(Graph g)
 {
 
+	GraphVertex* vtxAry = g.VertexArray;
+	GraphEdge* edge;
 
+
+	for(int i=0; i<g.V; i++)
+	{
+		printf("\n%d : ", vtxAry[i].vertexID);
+
+		edge = g.VertexArray[i].EdgeList;
+
+		while(edge != NULL)
+		{
+			printf("%d, ", edge->ToVertex);
+			edge = edge->NextEdge;
+		}
+
+	}
+
+	printf("\n");
 
 }
